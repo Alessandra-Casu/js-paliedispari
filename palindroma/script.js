bf007edbcda291d4e307af100a93b55a45127c68
Alessandra-Casu <alessandracasu919@gmail.com>
2023-04-20 13:24:14 +0200

    add struct

---
+++ palindroma/script.js
@@ -0,0 +1,13 @@
+/*
+Palindroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+ */
+
+/*
+    PSEUDOCODICE
+    richiedi la parola
+    memorizzala
+    controlla che la prima lettera corrisponda all'ultima con due cicli for in senso opposto
+ */
+  
